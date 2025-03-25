87ff19f2760f99ba1a6b09eaa6fdf1ec5f45b97d
Thomas Naughton <naughtont@ornl.gov>
2025-03-25 17:28:47 -0400

    add script to check/update rest config bits
    Signed-off-by: Thomas Naughton <naughtont@ornl.gov>

---
+++ update_rest_stuff.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# TJN: Hack to get all the rest bits in place on a new setup
+#      in case they are missing or goofed.
+
+set -e
+
+restart=false
+
+files="slurmdbd.conf slurm.conf rest.conf jwt.key"
+images="slurmctld slurmdbd slurmrestd"
+
+for img in $images ; do
+    echo "=== Image: $img ==="
+    for file in $files ; do
+        fqp_file="/etc/slurm/$file"
+        #echo " >> File: $fqp_file"
+
+        docker exec -ti $img ls -l $fqp_file || echo "Error: missing $img:$fqp_file"
+    done
+done
+
+#
+# If pass 'copy' arg to script, we will do following.
+#
+if [ "$1" == "copy" ] ; then
+    for img in $images ; do
+        echo "=== Image: $img ==="
+        for file in $files ; do
+            fqp_file="/etc/slurm/$file"
+            #echo " >>File: $fqp_file"
+
+            docker cp $file $img:$fqp_file                   || echo "Error: copy problem $img:$fqp_file"
+            docker exec -ti $img chown slurm.slurm $fqp_file || echo "Error: chown problem $img:$fqp_file"
+
+            if [ "$file" == "jwt.key" ] ; then
+                docker exec -ti $img chmod 600 $fqp_file     || echo "Error: chmod problem $img:$fqp_file"
+            fi
+            if [ "$file" == "slurmdbd.conf" ] ; then
+                docker exec -ti $img chmod 600 $fqp_file     || echo "Error: chmod problem $img:$fqp_file"
+            fi
+        done
+
+    done
+fi
