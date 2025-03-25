3918ed9dad863cb3eb4e6ca7d59bd7b528cdb6c0
Thomas Naughton <naughtont@ornl.gov>
2025-03-25 16:38:17 -0400

    try to get token from env and print JID/Status
    Signed-off-by: Thomas Naughton <naughtont@ornl.gov>

--- rest-testing/tiny-submit.py
+++ rest-testing/tiny-submit.py
@@ -1,9 +1,15 @@
 import requests
 import json
+import os
 
 SLURM_API_URL = "http://localhost:6820/slurm/v0.0.39/job/submit"
 
-MY_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDI5MTkxODUsImlhdCI6MTc0MjkxNzM4NSwic3VuIjoic2dydW5keSJ9.Yxx0YujM8NaWDonddVekPuLWSxLZelcImHEHnAblYZQ"
+if 'SLURM_JWT' in os.environ:
+    MY_TOKEN = os.environ.get('SLURM_JWT')
+    print(f"FOUND TOKEN IN ENV:\n {MY_TOKEN}")
+else:
+    MY_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDI5MzYzOTMsImlhdCI6MTc0MjkzNDU5Mywic3VuIjoic2dydW5keSJ9.HFZd56WeYMvX4jrx3D9OADo2TV24EOdIBFzZYNkXe2w"
+    print(f"TRY A DEFAULT TOKEN:\n {MY_TOKEN}")
 
 
 HEADERS = {
@@ -36,6 +42,11 @@ response = requests.post(SLURM_API_URL, headers=HEADERS, json=payload)
 if response.status_code == 200:
     print("Job submitted successfully!")
     print(json.dumps(response.json(), indent=4))
+    #data = json.loads(response.json())  # Convert JSON string to a dictionary
+    data = response.json()
+    jid = data['job_id']
+    jstat = data['result']['error_code']
+    print(f"DBG: Job_ID: {jid}  Job_Status: {jstat}  Response-Code: {response.status_code}")
 else:
     print(f"Failed to submit job: {response.status_code} {response.text}")
 
